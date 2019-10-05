537e2dacec1d83277e8f207f206eeea911ac4616
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-06 00:36:12 +0900

    :construction_worker:

--- scripts/slack/index.ts
+++ scripts/slack/index.ts
@@ -3,12 +3,15 @@ import { WebClient } from '@slack/web-api';
 /**
  * deploy 完了後の通知
  */
-async function main() {
+(async function main() {
   const web = new WebClient(process.env.SLACK_TOKEN);
 
+  const text = `
+  ${process.env.GITHUB_WORKFLOW} - ${process.env.GITHUB_ACTION}
+  ${process.env.TEXT}`;
+
   await web.chat.postMessage({
-    text: process.env.TEXT as string,
+    text,
     channel: process.env.CHANNEL as string,
   });
-}
-main();
+})();
