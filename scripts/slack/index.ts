76df03ff7670e72c32c05a2f0bde081bd373e32b
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-06 15:08:33 +0900

    :construction_worker:

--- scripts/slack/index.ts
+++ scripts/slack/index.ts
@@ -1,38 +1,38 @@
-// import { WebClient, Block } from '@slack/web-api';
+import { WebClient, Block } from '@slack/web-api';
 
-// /**
-//  * deploy 完了後の通知
-//  */
-// async function main(blocks: Block[], token: string) {
-//   // const text = `
-//   // ${process.env.GITHUB_WORKFLOW} - ${process.env.GITHUB_ACTION}
-//   // ${process.env.TEXT}`;
-//   const web = new WebClient(token);
+/**
+ * deploy 完了後の通知
+ */
+async function main(blocks: Block[], token: string) {
+  // const text = `
+  // ${process.env.GITHUB_WORKFLOW} - ${process.env.GITHUB_ACTION}
+  // ${process.env.TEXT}`;
+  const web = new WebClient(token);
 
-//   await web.chat.postMessage({
-//     channel: process.env.CHANNEL as string,
-//     text: '',
-//     blocks,
-//   });
-// }
+  await web.chat.postMessage({
+    channel: process.env.CHANNEL as string,
+    text: '',
+    blocks,
+  });
+}
 
-// const blocks = [
-//   {
-//     type: 'section',
-//     text: {
-//       type: 'mrkdwn',
-//       text: 'こんにちは世界:earth_asia: ',
-//     },
-//   },
-//   {
-//     type: 'context',
-//     elements: [
-//       {
-//         type: 'plain_text',
-//         text: 'https://www.dkrk-blog.net/',
-//         emoji: true,
-//       },
-//     ],
-//   },
-// ];
-// main(blocks, `${process.env.SLACK_TOKEN}`);
+const blocks = [
+  {
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: 'こんにちは世界:earth_asia: ',
+    },
+  },
+  {
+    type: 'context',
+    elements: [
+      {
+        type: 'plain_text',
+        text: 'https://www.dkrk-blog.net/',
+        emoji: true,
+      },
+    ],
+  },
+];
+main(blocks, `${process.env.SLACK_TOKEN}`);
