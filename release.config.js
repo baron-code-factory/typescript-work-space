b6b4b8825708d79876525dc2385d8ab9e380205e
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-08 02:54:39 +0900

    :lock:

--- release.config.js
+++ release.config.js
@@ -1,10 +1,10 @@
-import { promisify } from 'util';
+// import { promisify } from 'util';
 // import dateFormat from 'dateformat';
-import path from 'path';
+// import path from 'path';
 
-const readFileAsync = promisify(require('fs').readFile);
+// const readFileAsync = promisify(require('fs').readFile);
 
-const template = readFileAsync(path.join('./xxx', 'default-template.hbs'));
+// const template = readFileAsync(path.join('./xxx', 'default-template.hbs'));
 // const commitTemplate = readFileAsync(path.join('./xxx', 'commit-template.hbs'));
 
 export const plugins = [
@@ -17,7 +17,49 @@ export const plugins = [
         patch: [':bug:', ':ambulance:', ':lock:'],
       },
       releaseNotes: {
-        template,
+        template: `{{#if compareUrl}}
+        # [v{{nextRelease.version}}]({{compareUrl}}) ({{datetime "UTC:yyyy-mm-dd"}})
+        {{else}}
+        # v{{nextRelease.version}} ({{datetime "UTC:yyyy-mm-dd"}})
+        {{/if}}
+        
+        {{#with commits}}
+        {{#if sparkles}}
+        ## ✨ New Features
+        {{#each sparkles}}
+        - {{> commitTemplate}}
+        {{/each}}
+        {{/if}}
+        
+        {{#if bug}}
+        ## 🐛 Bug Fixes
+        {{#each bug}}
+        - {{> commitTemplate}}
+        {{/each}}
+        {{/if}}
+        
+        {{#if ambulance}}
+        ## 🚑 Critical Hotfixes
+        {{#each ambulance}}
+        - {{> commitTemplate}}
+        {{/each}}
+        {{/if}}
+        
+        {{#if lock}}
+        ## 🔒 Security Issues XXX
+        {{#each lock}}
+        - {{> commitTemplate}}
+        {{/each}}
+        {{/if}}
+        
+        {{#if boom}}
+        ## 💥 Breaking Changes
+        {{#each boom}}
+        - {{> commitTemplate}}
+        {{/each}}
+        {{/if}}
+        {{/with}}
+        `,
         // partials: { commitTemplate },
         // helpers: {
         //   datetime(format = 'UTC:yyyy-mm-dd') {
