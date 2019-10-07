165534719615988b00a52f3732f361230f1c5ad2
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-08 00:47:48 +0900

    feat(pencil): add 'graphiteWidth' option

--- release.config.js
+++ release.config.js
@@ -1,22 +1,22 @@
 module.exports = {
   plugins: [
-    // '@semantic-release/commit-analyzer',
+    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        preset: 'angular',
-        releaseRules: [
-          { type: ':memo:', scope: 'README', release: 'patch' },
-          { type: ':memo:', release: 'patch' },
-          { type: 'refactor', release: 'patch' },
-          { type: 'style', release: 'patch' },
-        ],
-        parserOpts: {
-          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
-        },
-      },
-    ],
+    // [
+    //   '@semantic-release/commit-analyzer',
+    //   {
+    //     // preset: 'angular',
+    //     releaseRules: [
+    //       { type: ':memo:', scope: 'README', release: 'patch' },
+    //       { type: ':memo:', release: 'patch' },
+    //       { type: 'refactor', release: 'patch' },
+    //       { type: 'style', release: 'patch' },
+    //     ],
+    //     parserOpts: {
+    //       noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+    //     },
+    //   },
+    // ],
     '@semantic-release/github',
 
     // 'semantic-release-gitmoji',
