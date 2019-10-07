13ff11d33d4a7adb53ea3d7e9ac05029dfccc88a
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-07 23:33:36 +0900

    :art:

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
+    // '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/github',
