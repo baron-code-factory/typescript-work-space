20d64b8ad784f41ebc5786a518bcab39372e43ec
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-07 20:40:54 +0900

    :bug:
    Closes #19

--- release.config.js
+++ release.config.js
@@ -1,8 +1,10 @@
-export const plugins = [
-  '@semantic-release/commit-analyzer',
-  '@semantic-release/release-notes-generator',
-  '@semantic-release/changelog',
-  '@semantic-release/github',
-  'semantic-release-gitmoji',
-  '@semantic-release/git',
-];
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    'semantic-release-gitmoji',
+    '@semantic-release/git',
+  ],
+};
