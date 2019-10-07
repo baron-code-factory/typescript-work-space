9d27c7593daec15e29659424b01807549071b812
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-08 03:11:12 +0900

    :lock:

--- release.config.js
+++ release.config.js
@@ -7,6 +7,50 @@
 // const template = readFileAsync(path.join('./xxx', 'default-template.hbs'));
 // const commitTemplate = readFileAsync(path.join('./xxx', 'commit-template.hbs'));
 
+const template = `{{#if compareUrl}}
+# [v{{nextRelease.version}}]({{compareUrl}}) ({{datetime "UTC:yyyy-mm-dd"}})
+{{else}}
+# v{{nextRelease.version}} ({{datetime "UTC:yyyy-mm-dd"}})
+{{/if}}
+
+{{#with commits}}
+{{#if sparkles}}
+## ✨ New Features
+{{#each sparkles}}
+- {{> commitTemplate}}
+{{/each}}
+{{/if}}
+
+{{#if bug}}
+## 🐛 Bug Fixes
+{{#each bug}}
+- {{> commitTemplate}}
+{{/each}}
+{{/if}}
+
+{{#if ambulance}}
+## 🚑 Critical Hotfixes
+{{#each ambulance}}
+- {{> commitTemplate}}
+{{/each}}
+{{/if}}
+
+{{#if lock}}
+## 🔒 Security Issues XXX
+{{#each lock}}
+- {{> commitTemplate}}
+{{/each}}
+{{/if}}
+
+{{#if boom}}
+## 💥 Breaking Changes
+{{#each boom}}
+- {{> commitTemplate}}
+{{/each}}
+{{/if}}
+{{/with}}
+`;
+
 module.exports = {
   plugins: [
     [
@@ -18,60 +62,7 @@ module.exports = {
           patch: [':bug:', ':ambulance:', ':lock:'],
         },
         releaseNotes: {
-          template: `{{#if compareUrl}}
-              # [v{{nextRelease.version}}]({{compareUrl}}) ({{datetime "UTC:yyyy-mm-dd"}})
-              {{else}}
-              # v{{nextRelease.version}} ({{datetime "UTC:yyyy-mm-dd"}})
-              {{/if}}
-              
-              {{#with commits}}
-              {{#if sparkles}}
-              ## ✨ New Features
-              {{#each sparkles}}
-              - {{> commitTemplate}}
-              {{/each}}
-              {{/if}}
-              
-              {{#if bug}}
-              ## 🐛 Bug Fixes
-              {{#each bug}}
-              - {{> commitTemplate}}
-              {{/each}}
-              {{/if}}
-              
-              {{#if ambulance}}
-              ## 🚑 Critical Hotfixes
-              {{#each ambulance}}
-              - {{> commitTemplate}}
-              {{/each}}
-              {{/if}}
-              
-              {{#if lock}}
-              ## 🔒 Security Issues XXX
-              {{#each lock}}
-              - {{> commitTemplate}}
-              {{/each}}
-              {{/if}}
-              
-              {{#if boom}}
-              ## 💥 Breaking Changes
-              {{#each boom}}
-              - {{> commitTemplate}}
-              {{/each}}
-              {{/if}}
-              {{/with}}
-              `,
-          // partials: { commitTemplate },
-          // helpers: {
-          //   datetime(format = 'UTC:yyyy-mm-dd') {
-          //     return dateFormat(new Date(), format);
-          //   },
-          // },
-          // issueResolution: {
-          //   template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
-          //   baseUrl: 'https://github.com',
-          //   source: 'github.com',
-          // },
+          template,
         },
       },
     ],
