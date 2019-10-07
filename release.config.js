c1780978863eab52a6975ee506505ca1c6883983
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-07 20:34:33 +0900

    :rotating_light: add gitmoji

--- release.config.js
+++ release.config.js
@@ -1,9 +1,8 @@
-module.exports = {
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/github',
-    '@semantic-release/git',
-  ],
-};
+export const plugins = [
+  '@semantic-release/commit-analyzer',
+  '@semantic-release/release-notes-generator',
+  '@semantic-release/changelog',
+  '@semantic-release/github',
+  'semantic-release-gitmoji',
+  '@semantic-release/git',
+];
