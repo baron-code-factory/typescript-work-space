f8b1d013bdee206c254cf986ad4f1ab6a3552a94
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-09 00:32:47 +0900

    :memo:

--- release.config.js
+++ release.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'gitmoji',
+        releaseRules: [{ type: ':docs:', release: 'patch' }],
+      },
+    ],
     '@semantic-release/release-notes-generator',
     // '@semantic-release/changelog',
     '@semantic-release/github',
