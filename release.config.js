55ee6711025add05594d27193c12a501d7e72b47
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-08 12:22:15 +0900

    fix(pencil): stop graphite breaking when too much pressure applied

--- release.config.js
+++ release.config.js
@@ -2,37 +2,9 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/github',
-      {
-        assets: [{ commits: [
-            {
-              commit: {
-                long: '68eb2c4d778050b0701136ca129f837d7ed494d2',
-                short: '68eb2c4'
-              },
-              tree: {
-                long: '7ab515d12bd2cf431745511ac4ee13fed15ab578',
-                short: '7ab515d'
-              },
-              author: {
-                name: 'Me',
-                email: 'me@email.com',
-                date: 2018-07-22T20:52:44.000Z
-              },
-              committer: {
-                name: 'Me',
-                email: 'me@email.com',
-                date: 2018-07-22T20:52:44.000Z
-              },
-              subject: 'feat: a new feature',
-              body: 'Description of the new feature',
-              hash: '68eb2c4d778050b0701136ca129f837d7ed494d2',
-              message: 'feat: a new feature\n\nDescription of the new feature',
-              committerDate: 2018-07-22T20:52:44.000Z
-            }
-           ] }],
-      },
-    ],
+    // '@semantic-release/changelog',
+    '@semantic-release/github',
+    // '@semantic-release/npm',
+    // '@semantic-release/git',
   ],
 };
