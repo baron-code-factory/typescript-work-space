b9d11f537fa02232669b059a5b5ba5bb61799adf
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-09 03:46:19 +0900

    :lipstick: (a) b

--- release.config.js
+++ release.config.js
@@ -3,20 +3,18 @@ module.exports = {
     [
       '@semantic-release/commit-analyzer',
       {
-        config: '@dior/conventional-changelog-gitmoji',
-        // preset: 'gitmoji',
-        // releaseRules: [
-        //   { type: ':memo:', release: 'patch' },
-        //   { type: ':lipstick:', release: 'patch' },
-        // ],
+        preset: 'gitmoji',
+        releaseRules: [
+          { type: ':memo:', release: 'patch' },
+          { type: ':lipstick:', release: 'patch' },
+        ],
       },
     ],
     // '@semantic-release/release-notes-generator',
     [
       '@semantic-release/release-notes-generator',
       {
-        config: '@dior/conventional-changelog-gitmoji',
-        // preset: 'gitmoji',
+        preset: 'gitmoji',
       },
     ],
     '@semantic-release/github',
