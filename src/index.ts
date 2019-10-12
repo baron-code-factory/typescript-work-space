aae9fcc8aa903e8bc0d23507889e7bfc54707ce6
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-12 14:13:44 +0900

    :package: add uml

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,33 @@ export function main() {
 }
 
 /**
- * さぶちゃん
+ * Hoge is sample class for example of `typedoc-plugin-mermaid`.
+ *
+ * @mermaid Make TypeDoc easy to use with mermaid.js
+ * graph TB
+ *   mermaid.js --> TypeDoc;
+ *
+ * @nomnoml Alice greet to Jone
+ * [Pirate|eyeCount: Int|raid();pillage()|
+ *   [beard]--[parrot]
+ *   [beard]-:>[foul mouth]
+ * ]
+ *
+ * [<abstract>Marauder]<:--[Pirate]
+ * [Pirate]- 0..7[mischief]
+ * [jollyness]->[Pirate]
+ * [jollyness]->[rum]
+ * [jollyness]->[singing]
+ * [Pirate]-> *[rum|tastiness: Int|swig()]
+ * [Pirate]->[singing]
+ * [singing]<->[rum]
+ *
+ * [<start>st]->[<state>plunder]
+ * [plunder]->[<choice>more loot]
+ * [more loot]->[st]
+ * [more loot] no ->[<end>e]
+ *
+ * [<actor>Sailor] - [<usecase>shiver me;timbers]
  */
 export function sub() {
   return {
